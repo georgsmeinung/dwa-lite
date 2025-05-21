79ef159d8de12f47c206dd87ecf4fcfd4ba4dfc6
MartinCancelas <martin.cancelas1992@gmail.com>
2025-05-21 08:40:11 -0300

    para corregir duplicado de codigo

--- transform/20_transform_stg_to_dwa.py
+++ transform/20_transform_stg_to_dwa.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ejecutar_sqlite_script_env.py
 import os
 from dotenv import load_dotenv
@@ -21,28 +20,4 @@ print("→ Ejecutando SQL:")
 print("  SQL:", SCRIPT_FILE_PATH)
 print("  DB :", DB_PATH)
 
-=======
-# ejecutar_sqlite_script_env.py
-import os
-from dotenv import load_dotenv
-from sqlite_utils import ejecutar_sql
-
-# Ruta del directorio donde está este script (no el CWD desde donde lo llamás)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Cargar .env relativo a este script también
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
-
-# Cargar variable 
-DB_PATH = os.getenv("DB_PATH", "")
-SCRIPT_FILE_PATH = os.getenv("20_SQL_FILE_PATH", "")
-# Resolver rutas relativas al script
-ruta_db = os.path.join(BASE_DIR, DB_PATH)
-archivo_sql = os.path.join(BASE_DIR, SCRIPT_FILE_PATH)
-
-print("→ Ejecutando SQL:")
-print("  SQL:", SCRIPT_FILE_PATH)
-print("  DB :", DB_PATH)
-
->>>>>>> a135a941dddd54b71f3fd57a0cfbc1fce3fbbaf3
 ejecutar_sql(archivo_sql, ruta_db)
--- transform/sqlite_utils.py
+++ transform/sqlite_utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # sqlite_utils.py
 import sqlite3
 import os
@@ -26,32 +25,3 @@ def ejecutar_sql(archivo_sql, ruta_db):
     finally:
         if conn:
             conn.close()
-=======
-# sqlite_utils.py
-import sqlite3
-import os
-
-def ejecutar_sql(archivo_sql, ruta_db):
-    if not os.path.isfile(archivo_sql):
-        print(f"❌ Error: El archivo SQL '{archivo_sql}' no existe.")
-        return
-
-    if not os.path.isfile(ruta_db):
-        print(f"❌ Error: La base de datos SQLite '{ruta_db}' no existe.")
-        return
-
-    try:
-        with open(archivo_sql, 'r', encoding='utf-8') as f:
-            sql_script = f.read()
-
-        conn = sqlite3.connect(ruta_db)
-        cursor = conn.cursor()
-        cursor.executescript(sql_script)
-        conn.commit()
-        print(f"ℹ️ Script ejecutado correctamente.")
-    except sqlite3.Error as e:
-        print(f"❌ Error de SQLite: {e}")
-    finally:
-        if conn:
-            conn.close()
->>>>>>> a135a941dddd54b71f3fd57a0cfbc1fce3fbbaf3
